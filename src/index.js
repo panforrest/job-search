d409bd8cc51d0a92ae4fa72dabd208b7d1f76278
panforrest <guoqianp@gmail.com>
2017-12-25 10:38:32 -0500

    re-compile bundle to see Job Search App

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import store from './stores'
+import { Provider } from 'react-redux'
+// import Intro from './components/Intro'
+
+
+const app = (
+	<div>
+        <h1>Job Search App!</h1>
+	</div>
+)
+
+
+ReactDOM.render(app, document.getElementById('root'))
